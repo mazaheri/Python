15deb058552d0ada9daad9244f7599e763810c16
Pourya Mazaheri <mazaheri.pourya@gmail.com>
2023-06-30 12:40:57 +0200

    craiglist_crawler

--- MongoDB/storage.py
+++ MongoDB/storage.py
@@ -1,3 +1,33 @@
+import json
+from abc import ABC, abstractmethod
+
+from mongo import MongoDatabase
+
+
+class StorageAbstract(ABC):
+
+    @abstractmethod
+    def store(self, data, *args):
+        pass
+
+
 class MongoStorage(StorageAbstract):
-    def __init__(slef):
-        self.mongo = 
+
+    def __init__(self):
+        self.mongo = MongoDatabase()
+
+    def store(self, data, collection, *args):
+        collection = getattr(self.mongo.database, collection)
+        if isinstance(data, list) and len(data) > 1:
+            collection.insert_many(data)
+        else:
+            collection.insert_one(data)
+
+
+class FileStorage(StorageAbstract):
+
+    def store(self, data, filename, *args):
+        filename = filename + '-' + data['post_id']
+        with open(f'fixtures/adv/{filename}.json', 'w') as f:
+            f.write(json.dumps(data))
+        print(f'fixtures/adv/{filename}.json')
